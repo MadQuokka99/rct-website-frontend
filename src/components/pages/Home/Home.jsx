844b18239986be580c5847da52619c1a80ffae96
Alex Gheorghe <alexandrug@ensemble.com>
2022-12-06 22:43:19 +0200

    feature/W3RCT-4 Added menu elements. Worked on top language bar

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -7,6 +7,7 @@ import Button from "@mui/material/Button";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
 
 const { homepage: homepageStrings } = strings;
 
@@ -20,57 +21,93 @@ const Home = () => {
     setAnchorEl(null);
     console.log(e);
   };
+
+  const handleSearchClick = () => {
+    console.log("Add search functionality: modal smth smth");
+  };
+
   return (
     <>
       <Helmet>
         <title>{homepageStrings.title}</title>
       </Helmet>
+      <div className="languageHeader">
+        <button className="btn">RO</button>
+        <Button>EN</Button>
+      </div>
 
       <div className="header">
         <img src={Logo} className="logoImage" alt="Website logo" />
-        <Button
-          id="basic-button"
-          aria-controls={open ? "basic-menu" : undefined}
-          aria-haspopup="true"
-          aria-expanded={open ? "true" : undefined}
-          onClick={handleClick}
-        >
-          {<MenuIcon fontSize="large" className="hamburger" />}
-        </Button>
-        <Menu
-          id="basic-menu"
-          anchorEl={anchorEl}
-          open={open}
-          onClose={handleClose}
-          MenuListProps={{
-            "aria-labelledby": "basic-button",
-          }}
-        >
-          <MenuItem
-            onClick={(e) => {
-              handleClose(e);
-              console.log("Here we will navigate");
+        <div className="menu-left-side">
+          <Button
+            id="basic-button"
+            aria-controls={open ? "basic-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+          >
+            {<SearchIcon className="search-icon" onClick={handleSearchClick} />}
+          </Button>
+
+          <Button
+            id="basic-button"
+            aria-controls={open ? "basic-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+            onClick={handleClick}
+          >
+            {<MenuIcon fontSize="large" className="hamburger-menu-icon" />}
+          </Button>
+          <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              "aria-labelledby": "basic-button",
             }}
           >
-            Profile
-          </MenuItem>
-          <MenuItem
-            onClick={(e) => {
-              handleClose(e);
-              console.log("Here we will navigate");
-            }}
-          >
-            My account
-          </MenuItem>
-          <MenuItem
-            onClick={(e) => {
-              handleClose(e);
-              console.log("Here we will navigate");
-            }}
-          >
-            Logout
-          </MenuItem>
-        </Menu>
+            <MenuItem
+              onClick={(e) => {
+                handleClose(e);
+                console.log("Here we will navigate");
+              }}
+            >
+              {homepageStrings.menuButtons.home}
+            </MenuItem>
+            <MenuItem
+              onClick={(e) => {
+                handleClose(e);
+                console.log("Here we will navigate");
+              }}
+            >
+              {homepageStrings.menuButtons.news}
+            </MenuItem>
+            <MenuItem
+              onClick={(e) => {
+                handleClose(e);
+                console.log("Here we will navigate");
+              }}
+            >
+              {homepageStrings.menuButtons.schedule}
+            </MenuItem>
+            <MenuItem
+              onClick={(e) => {
+                handleClose(e);
+                console.log("Here we will navigate");
+              }}
+            >
+              {homepageStrings.menuButtons.contact}
+            </MenuItem>
+            <MenuItem
+              onClick={(e) => {
+                handleClose(e);
+                console.log("Here we will navigate");
+              }}
+            >
+              {homepageStrings.menuButtons.aboutUs}
+            </MenuItem>
+          </Menu>
+        </div>
       </div>
       <div className="content"></div>
       <div className="footer"></div>
--- src/config/strings/homepage.js
+++ src/config/strings/homepage.js
@@ -3,6 +3,13 @@ export default {
   en: {
     homepage: {
       title: "Home",
+      menuButtons: {
+        home: "Home",
+        news: "News",
+        schedule: "Schedule",
+        contact: "Contact",
+        aboutUs: "About Us",
+      },
     },
   },
 };
