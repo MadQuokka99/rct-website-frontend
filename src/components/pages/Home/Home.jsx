ada846a354e1c2febd95cff39aeb21a6d2a37ae7
Alex Gheorghe <alexandrug@ensemble.com>
2022-12-06 19:32:04 +0200

    feature/W3RCT-4 Building the header of the page and basic page elem

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,17 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 import { Helmet } from "react-helmet-async";
 import strings from "../../../config/strings";
+import "./Home.css";
+import Logo from "../../../utils/rct-logo.png";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const { homepage: homepageStrings } = strings;
 
 const Home = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = (e) => {
+    setAnchorEl(null);
+    console.log(e);
+  };
   return (
-    <div>
+    <>
       <Helmet>
         <title>{homepageStrings.title}</title>
       </Helmet>
-      <h1>HomePage</h1>
-    </div>
+
+      <div className="header">
+        <img src={Logo} className="logoImage" alt="Website logo" />
+        <Button
+          id="basic-button"
+          aria-controls={open ? "basic-menu" : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? "true" : undefined}
+          onClick={handleClick}
+        >
+          {<MenuIcon fontSize="large" className="hamburger" />}
+        </Button>
+        <Menu
+          id="basic-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          MenuListProps={{
+            "aria-labelledby": "basic-button",
+          }}
+        >
+          <MenuItem
+            onClick={(e) => {
+              handleClose(e);
+              console.log("Here we will navigate");
+            }}
+          >
+            Profile
+          </MenuItem>
+          <MenuItem
+            onClick={(e) => {
+              handleClose(e);
+              console.log("Here we will navigate");
+            }}
+          >
+            My account
+          </MenuItem>
+          <MenuItem
+            onClick={(e) => {
+              handleClose(e);
+              console.log("Here we will navigate");
+            }}
+          >
+            Logout
+          </MenuItem>
+        </Menu>
+      </div>
+      <div className="content"></div>
+      <div className="footer"></div>
+    </>
   );
 };
 
